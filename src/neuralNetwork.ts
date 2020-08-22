28f66afe4649d88a59fd4f9182767a8fce750dd8
Jesus Prieto <jesus.prieto@moixa.com>
2020-08-22 22:06:03 +0000

    Training function

--- src/neuralNetwork.ts
+++ src/neuralNetwork.ts
@@ -1,6 +1,15 @@
 import * as tf from "@tensorflow/tfjs";
+import { Board } from "./board";
+import * as BoardM from "./board";
+import * as TE from "fp-ts/lib/TaskEither";
+import { TaskEither } from "fp-ts/lib/TaskEither";
 
-export const mkModel = (boardDim: number): tf.Sequential => {
+type QValues = Array<number>;
+type Model = tf.Sequential;
+
+type Error = "training_error";
+
+export const mkModel = (boardDim: number): Model => {
   const model = tf.sequential({
     layers: [
       // Receives the binary-encoded representation of each cell
@@ -20,4 +29,30 @@ export const mkModel = (boardDim: number): tf.Sequential => {
   });
 
   return model;
+};
+
+export const train = (
+  b: Board,
+  qvalues: QValues,
+  model: Model
+): TaskEither<Error, Model> => {
+  return TE.tryCatch(
+    () =>
+      model
+        .fit(tf.tensor(BoardM.toBinaryArray(b)), tf.tensor(qvalues), {
+          shuffle: true,
+          epochs: 20,
+          callbacks: {
+            onEpochEnd: (epoch, logs) => {
+              console.log("Epoch " + epoch);
+              console.log("Loss: " + logs.loss + " accuracy: " + logs.acc);
+            }
+          }
+        })
+        .then(() => model),
+    err => {
+      console.log("[ERROR] Training error", err);
+      return "training_error";
+    }
+  );
 };
